db3f4c92e1af4166744fbebac2e09e0268e884d7
Rahul <rmisal@uw.edu>
2020-05-17 18:50:39 -0700

    Save 4

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
-function click() {
-  alert(this.textContent + "was clicked!");
-  this.disabled = true;
-}
+
+"use strict";
+(function() {
+
+  window.addEventListener("load", init);
+
+  /** This creates the functionality of all of the buttons on the page */
+  function init() {
+    let buttons = document.querySelectorAll(".button");
+    for (let i = 0; i < 4; i++) {
+      buttons[i].onclick = click;
+    }
+  }
+
+  /** Click function */
+  function click() {
+    alert(this.textContent + " was clicked!");
+    this.disabled = true;
+  }
+
+
+})();
