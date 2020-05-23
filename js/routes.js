6555be26abd9acbe1c5737302ab44c06bcd5f31c
Kevin Wang <kevin.wang.or@gmail.com>
2020-05-22 21:58:19 -0700

    added routing

--- js/routes.js
+++ js/routes.js
@@ -1,23 +1,22 @@
 const routes = [
-    {
-      path: '/benefits',
-      template: `<h1>Benefits!</h1>`,
-      //getTemplate: (params) => '<h3>Learn about the benefits!</h3>'
-    },
-    {
-      path: '/rainwaterSystem',
-      getTemplate: (params) => '<h1>Explore the rainwater system!</h1>',
-    },
-    {
-      path: '/rainCycle',
-      getTemplate: (params) => '<h1>Explore the rain cycle!</h1>',
-    },
-    {
-        path: '/rainwaterGame',
-        getTemptlate: (params) => '<h2>Play the rainwater game!</h2>'
-    },
-    {
-      path: '/products/:productId',
-      getTemplate: (params) => `<h1>Product ${params.productId}</h1>`,
-    },
-  ];
+  {
+    path: '/',
+    template: '<h1></h1>'
+  },
+  {
+    path: '/benefits',
+    template: '<h1></h1>',
+  },
+  {
+    path: '/rainwaterSystem',
+    template: '<h1></h1>',
+  },
+  {
+    path: '/rainCycle',
+    template: '<h1></h1>',
+  },
+  {
+    path: '/rainwaterGame',
+    template: '<h1></h1>',
+  },
+];
