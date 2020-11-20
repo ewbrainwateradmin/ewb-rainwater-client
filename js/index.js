9049662b6bb24aa29ac0809b1b811ce6c1a90254
fatimah7 <fatimah7@uw.edu>
2020-11-19 18:07:05 -0800

    added base of index.js file for new routing system

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
+"use strict"
+​
+const express = require("express")
+const app = express()
+​
+const port = process.env.PORT || "80"
+​
+app.get('/', (req, res) => {
+  // Do a thing
+})
+​
+app.listen(port, () => {
+  console.log(`Listening on :${port}`)
+})
+
+
+
 // Initialise an instance of our router class.
 //const router = require('./router')
 
 //router.router
-const router = new Router(routes);
+//const router = new Router(routes);
