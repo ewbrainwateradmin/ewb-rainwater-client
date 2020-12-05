f636cf5323f3c6d45554721f48628b581bdacdc4
sonik <sonikat@uw.edu>
2020-12-04 17:22:19 -0800

    added to index.js routing file

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict"
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set static folder
+// for static server, routes end in .html (ie http://localhost:80/home.html)
+app.use(express.static(path.join(__dirname, 'static')));
+
+// set up main page
+app.get('/',function(req, res){
+    res.sendFile(__dirname+'/static/index.html')
+})
+
+const PORT = process.env.PORT || 5500
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
