da7c0d7b284d7f5cfce700ec8a9143b7fe21291b
Evan <>
2021-01-15 15:11:31 -0800

    Took username out of docker files

--- docker_commands_client.bat
+++ docker_commands_client.bat
@@ -1,5 +1,3 @@
 docker rm -f ewb-rainwater-client
-docker build -t kayakcat/ewb-rainwater-client .
-docker push kayakcat/ewb-rainwater-client
-docker pull kayakcat/ewb-rainwater-client
-docker create -p 5500:5500 --name ewb-rainwater-client kayakcat/ewb-rainwater-client
+docker build -t ewb-rainwater-client .
+docker create -p 5500:5500 --name ewb-rainwater-client ewb-rainwater-client
