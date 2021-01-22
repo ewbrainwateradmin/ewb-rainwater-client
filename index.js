5f45b4ac459a5e9c0a5d74b162f35313b13d733d
Evan <>
2021-01-21 18:29:23 -0800

    Got the code snippet for container communication

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 "use strict"
 const express = require('express');
 const path = require('path');
+const got = require('got');
 const app = express();
 
 // Set static folder
 // for static server, routes end in .html (ie http://localhost:5500/home.html)
 app.use(express.static(path.join(__dirname, 'static')));
+
+(async () => {
+  try {
+    // Take out manual port entry
+    const response = await got('http://ewb-rainwater-server:80/current_dollars')
+    console.log(response.body)
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
 
 // set up main page
 app.get('/',function(req, res){
