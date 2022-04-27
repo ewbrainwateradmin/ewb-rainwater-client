f9ac3264e1fa9e7283a379e8c809c80fab8ec172
Evan Truesdale <kayakcat@truecrew.net>
2022-04-26 18:21:32 -0700

    updated for arm64 architectures

--- database/docker_commands_database.sh
+++ database/docker_commands_database.sh
@@ -1,5 +1,14 @@
 #!/bin/bash/
 
 docker rm -f ewb-rainwater-database
-docker pull --platform linux/x86_64/v8 mysql
-docker create -p 3306:3306 --name ewb-rainwater-database --health-cmd="mysql -u root -proot -e 'status' &> /dev/null" -e MYSQL_ROOT_PASSWORD=ewb2020 mysql
+ARCH=$(uname -m)
+echo $ARCH
+if [ "$ARCH" == 'arm64' ]; then
+	echo "arm64 was detected"
+	docker pull --platform linux/arm64 mysql:oracle
+	docker create -p 3306:3306 --name ewb-rainwater-database --health-cmd="mysql -u root -proot -e 'status' &> /dev/null" -e MYSQL_ROOT_PASSWORD=ewb2020 mysql:oracle
+else
+	echo "other was detected"
+	docker pull --platform linux/amd64 mysql:latest
+	docker create -p 3306:3306 --name ewb-rainwater-database --health-cmd="mysql -u root -proot -e 'status' &> /dev/null" -e MYSQL_ROOT_PASSWORD=ewb2020 mysql:latest
+fi
